77b2a5abf837bce4bc6982f5e7e6b5b590ddd735
qingrong.zhao <qingrong.zhao@urbanic.com>
2026-05-29 19:58:08 +0800

    seo: add crawl-delay for scrapers and disallow /api in robots.txt

--- app/robots.ts
+++ app/robots.ts
@@ -3,7 +3,23 @@ import { BASE_URL } from '@/lib/config'
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: { userAgent: '*', allow: '/' },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/api/'],
+      },
+      {
+        userAgent: 'AhrefsBot',
+        allow: '/',
+        crawlDelay: 10,
+      },
+      {
+        userAgent: 'SemrushBot',
+        allow: '/',
+        crawlDelay: 10,
+      },
+    ],
     sitemap: `${BASE_URL}/sitemap.xml`,
   }
 }
